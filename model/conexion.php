78b28ac52691ba02b3a8a36d646a2c45d3fc0adb
Omar <omartox@gmail.com>
2020-04-02 14:41:20 -0600

    actualizacion de la base de datos

--- model/conexion.php
+++ model/conexion.php
@@ -8,7 +8,7 @@
 	define('DB_HOST','localhost');
 	define('DB_USER','root');
 	define('DB_PASS','');
-	define('DB_NAME','csa_db');
+	define('DB_NAME','csa_db2');
 	# conectare la base de datos
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(!$con){
